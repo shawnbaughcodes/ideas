87463d47bbc01e6aaf1ef773df98cb3481cb0288
Shawn Baugh II <shawnbaughcreative@gmail.com>
2017-07-14 15:35:39 -0500

    Completed edits on application. Styled each page with materialize. Added materialize css cdn. fixed minor spelling errors. fixed user queries for each page.

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -1,15 +1,24 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://coffeescript.org/
+$(document).on('click', '#create_button', function(){
+    $('div.ideas').fadeOut('fast', function() {
+        $('div.create_idea').fadeIn('fast');
+    })
+})
+$(document).on('click', '#cancel', function(){
+    $('div.create_idea').fadeOut('fast', function() {
+        $('div.ideas').fadeIn('fast');
+    })
+})
 $(document).ready(function() {
      // BUTTON TO CREATE IDEAS
-     $('#create_button').click(function (){
-         $('div.ideas').fadeOut('fast', function() {
-             $('div.create_idea').fadeIn('fast', function() {
-                 //Stuff to do *after* the animation takes place
-             })
-         })
-     });
+    //  $('#create_button').click(function (){
+    //      $('div.ideas').fadeOut('fast', function() {
+    //          $('div.create_idea').fadeIn('fast', function() {
+    //          })
+    //      })
+    //  });
     //  POST IDEAS
      $('.idea_create').submit(function(){
          $('.create_idea').fadeOut('slow', function() {
